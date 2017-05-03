0afafffc7666a0d4bbcaa740bcb82d4fade48e81
Pascal Birchler <hello@pascalbirchler.ch>
2017-05-03 18:34:14 +0200

    Remove commented out code
    Use Git if you desperately need it again

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -214,43 +214,7 @@ class Plugin {
 		<?php
 	}
 
-	public function track_contact_form7( $form ) {
-		/*
-
-     // Demo code from Drupal
-    if (valid_email_address($component_data['e_mail'][0])) {
-        $data['email'] = $component_data['e_mail'][0];
-    }
-    if (valid_email_address($component_data['email'][0])) {
-        $data['email'] = $component_data['email'][0];
-    }
-    if ($component_data['company_name'][0]) {
-        $data['company_name'] = $component_data['company_name'][0];
-    }
-    if ($component_data['company'][0]) {
-        $data['company_name'] = $component_data['company'][0];
-    }
-    if ($component_data['phone'][0]) {
-        $data['phone'] = $component_data['phone'][0];
-    }
-    if ($component_data['first_name'][0]) {
-        $data['first_name'] = $component_data['first_name'][0];
-    }
-    if ($component_data['last_name'][0]) {
-        $data['last_name'] = $component_data['last_name'][0];
-    }
-
-
-    $oi = new OI(variable_get('openinbound_tracking_id'), variable_get('openinbound_api_key'));
-    $oi->updateContact($_COOKIE['_oi_contact_id'], $data);
-
-    $properties = array();
-    $properties['title'] = 'Form submission by '.$data['email'].' - '.$node->title;
-    $properties['event_type'] = 'submission';
-    $properties['raw'] = json_encode($component_data);
-    $oi->addEvent($_COOKIE['_oi_contact_id'], $properties);
-    */
-	}
+	public function track_contact_form7( $form ) {}
 
 	/**
 	 * Registers the Gravity Forms feed add-on.
